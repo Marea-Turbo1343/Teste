e012ee398a68a1400171e1056b972d9a48b51c89
Thiago Culau <thiago.culau1@outlook.com>
2024-05-07 12:24:03 -0300

    Add project files.

---
+++ Teste/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste DESSA MERDA QUE NAO FUNCIONA CAPETA DO INFERNO");
+        }
+    }
+}
